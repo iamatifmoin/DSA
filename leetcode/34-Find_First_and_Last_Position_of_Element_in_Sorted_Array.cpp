0bd495a1e1b502c6395814b6633688256e5d1a15
iamatifmoin <atifmoin3@gmail.com>
2025-03-22 22:51:49 +0530

    34, 704

---
+++ leetcode/34-Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,48 @@
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums is a non-decreasing array.
+// -109 <= target <= 109
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int firstIndex = -1, lastIndex = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == target && firstIndex == -1)
+                firstIndex = i;
+            if (nums[i] == target && firstIndex != -1)
+                lastIndex = max(i, lastIndex);
+        }
+
+        if (firstIndex == -1 && lastIndex == -1)
+            return {-1, -1};
+        else
+            return {firstIndex, lastIndex};
+    }
+};
